f656a4b75ba0f5c81b44d0a772c624e3f9305f6b
Phil Garner <pgarner@gmx.ch>
2019-02-02 15:51:38 +0100

    Add ICASSP 2019; fix html.sh to deal with separate strings

--- html.sh
+++ html.sh
@@ -2,19 +2,24 @@
 
 html=$HOME/doc/github.io/_pubs
 
-# Use a relative css path so it work independent of baseurl
+# Use a relative css path so it works independent of baseurl
 opts=(
     -css assets/stylish.css
     -r
     -d
 )
 
-bibtex2html $opts -t "Philip N. Garner: Publications" \
-            png-pubs.bib
-bibtex2html $opts -t "Philip N. Garner: Patents and published applications" \
-            png-pats.bib
-bibtex2html $opts -t "Philip N. Garner: Technical reports" \
-            png-tech.bib
+cat png-strs.bib png-pubs.bib \
+    | bibtex2html $opts -t "Philip N. Garner: Publications" \
+                  -o png-pubs
+            
+cat png-strs.bib png-pats.bib \
+    | bibtex2html $opts -t "Philip N. Garner: Patents and published applications" \
+                  -o png-pats
+
+cat png-strs.bib png-tech.bib \
+    | bibtex2html $opts -t "Philip N. Garner: Technical reports" \
+                  -o png-tech
 
 echo Copying to $html
 cp *.html $html
